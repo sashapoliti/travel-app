982028e4318a16484c654501e639b50ffc4bee64
sashapoliti <sas.politi@gmail.com>
2024-08-30 23:01:09 +0200

    header done, remove useless components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -13,12 +12,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped lang="scss">
 header {
+  margin-bottom: 1.5rem;
   line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
   h1 {
-    font-size: 3rem;
+    font-size: 2rem;
     font-weight: 700;
-    margin-bottom: 2rem;
   }
 }
 
@@ -50,32 +49,5 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
